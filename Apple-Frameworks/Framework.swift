b2fe7fba9c463acb89444d58a4f7c9215521d1c6
PreetamG <preetamgodase944@gmail.com>
2024-07-15 11:53:53 +0530

    Add lazy grid view to display grid of frameworks

--- Apple-Frameworks/Framework.swift
+++ Apple-Frameworks/Framework.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Framework {
+struct Framework: Identifiable {
     let id = UUID()
     let name: String
     let imageName: String
--- Apple-Frameworks/Home.swift
+++ Apple-Frameworks/Home.swift
@@ -8,8 +8,19 @@
 import SwiftUI
 
 struct Home: View {
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(),
+        GridItem()
+    ]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            LazyVGrid(columns: columns) {
+                ForEach(MockData.frameworks) { framework in
+                    FrameworkTileView()
+                }
+            }
+        }
     }
 }
 
