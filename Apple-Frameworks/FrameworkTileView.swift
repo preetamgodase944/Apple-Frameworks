39ca2b38e0087a3242c6ae32bc7eb086916cfa65
PreetamG <preetamgodase944@gmail.com>
2024-07-15 11:48:20 +0530

    configure FrameworkTIleView

--- Apple-Frameworks/FrameworkTileView.swift
+++ Apple-Frameworks/FrameworkTileView.swift
@@ -10,7 +10,15 @@ import SwiftUI
 struct FrameworkTileView: View {
     var body: some View {
         VStack {
-            
+            Image("arkit")
+                .resizable()
+                .frame(width: 90,height: 90)
+                .aspectRatio(contentMode: .fit)
+            Text("ARKit")
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.5)
         }
     }
 }
